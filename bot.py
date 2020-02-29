9d53dcdd9adacf2f88c35c019cfb977a763a00ff
Harold Mendoza <hgmendoza25@gmail.com>
2020-02-29 11:07:23 +0800

    added member count

--- bot.py
+++ bot.py
@@ -18,15 +18,18 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.command(name='attendance', help='Pass voice channel as an argument')
 async def attendance(ctx, channel):
+    member_count = 0
     guild = ctx.guild
     voice_channel = discord.utils.get(guild.channels, name=channel, type=discord.ChannelType.voice)
 
     if not voice_channel.members:
         await ctx.send('No members inside this voice chat!')
     else:
+        for member in voice_channel.members:
+            member_count += 1
         await ctx.send(f'This is the list of Druchii members who attended VC on {datetime.date.today()} : \n')
-        await ctx.send("\n".join([member.name for member in voice_channel.members]))
-
+        await ctx.send("\n".join([member.display_name for member in voice_channel.members]))
+        await ctx.send(f'Member count: {member_count}')
 
 @attendance.error
 async def info_error(ctx, error):
